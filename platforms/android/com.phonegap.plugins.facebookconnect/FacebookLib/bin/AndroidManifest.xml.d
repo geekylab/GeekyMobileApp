822c949869b2443d2adb07c911f97821c3755b0a
johna1203 <johnathan@kodokuman.com>
2014-11-04 12:15:59 -0200

    facebook plugin.

---
+++ platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/johna/src/GeekyMenu/mobile/platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/bin/AndroidManifest.xml : \
+/home/johna/src/GeekyMenu/mobile/platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/AndroidManifest.xml \
